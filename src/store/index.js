a8649efeadd5c77b3e625ef072b06d7c314e88e0
Amon Sawamura <amon.sawamura@gmail.com>
2019-11-30 22:16:48 +0900

    add works store

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import { works } from './modules/works';
 
 Vue.use(Vuex);
 
@@ -7,5 +8,7 @@ export default new Vuex.Store({
   state: {},
   mutations: {},
   actions: {},
-  modules: {},
+  modules: {
+    works,
+  },
 });
---
+++ src/store/modules/works.js
@@ -0,0 +1,34 @@
+import fetch from 'node-fetch';
+
+export const works = {
+  namespaced: true,
+  state: {
+    items: [],
+    loading: true,
+  },
+  mutations: {
+    setItems(state, items) {
+      state.items = items;
+    },
+    finishLoading(state) {
+      state.loading = false;
+    },
+  },
+  actions: {
+    async getItems({ commit }) {
+      const url = `${process.env.VUE_APP_CMS_API_ENDPOINT}/works`;
+      const response = await fetch(url, {
+        headers: { 'X-API-KEY': process.env.VUE_APP_CMS_X_API_KEY },
+      });
+      console.log(response);
+      const json = await response.json();
+      const items = json.contents.map((item) => ({
+        title: item.title || null,
+        thumbnail: item.thumbnail ? item.thumbnail.url : null,
+        category: item.category ? item.category.name : null,
+      }));
+      commit('setItems', items);
+      commit('finishLoading');
+    },
+  },
+};
