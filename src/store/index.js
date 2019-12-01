40a8329f80995d934c893aa5172c09a73895418d
Amon Sawamura <amon.sawamura@gmail.com>
2019-12-01 17:36:43 +0900

    store strict mode

--- src/store/index.js
+++ src/store/index.js
@@ -5,6 +5,7 @@ import { works } from './modules/works';
 Vue.use(Vuex);
 
 export default new Vuex.Store({
+  strict: process.env.NODE_ENV !== 'production',
   state: {},
   mutations: {},
   actions: {},
