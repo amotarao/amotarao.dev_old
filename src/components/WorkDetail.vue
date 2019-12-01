683f00964ee8a4e6acc6d1515166a8334ef0710b
Amon Sawamura <amon.sawamura@gmail.com>
2019-12-01 17:44:37 +0900

    add Work page

---
+++ src/components/WorkDetail.vue
@@ -0,0 +1,140 @@
+<template>
+  <div class="work-detail">
+    <div class="image-wrap">
+      <twitter-icon v-if="category === 'twitter'" class="icon" />
+      <img v-else :src="thumbnail" :srcset="thumbnailSrcset" class="image" alt="" />
+      <h2>{{ title }}</h2>
+    </div>
+    <ul class="link-list">
+      <li class="link-item">
+        <link-button href="https://twitter.com/" type="website">サービスページ</link-button>
+      </li>
+      <li class="link-item">
+        <link-button href="https://twitter.com/" type="github">ソースコード</link-button>
+      </li>
+    </ul>
+    <main class="main">
+      <p>ここにサービスの説明を入れますここにサービスの説明を入れますここにサービスの説明を入れます</p>
+      <p>ここにサービスの説明を入れますここにサービスの説明を入れますここにサービスの説明を入れます</p>
+      <div class="img-wrap">
+        <img src="https://picsum.photos/600/200" />
+      </div>
+      <p>たまに画像もはいります</p>
+      <p>ここにサービスの説明を入れますここにサービスの説明を入れますここにサービスの説明を入れます</p>
+      <p>ここにサービスの説明を入れますここにサービスの説明を入れますここにサービスの説明を入れます</p>
+    </main>
+  </div>
+</template>
+
+<script>
+import TwitterIcon from './icons/TwitterIcon';
+import LinkButton from './atoms/LinkButton';
+
+export default {
+  components: {
+    TwitterIcon,
+    LinkButton,
+  },
+  props: {
+    id: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    thumbnail: {
+      type: String,
+      default: '',
+    },
+    category: {
+      type: String,
+      default: 'default',
+      validator(value) {
+        return ['default', 'webapp', 'website', 'twitter'].includes(value);
+      },
+    },
+  },
+  computed: {
+    thumbnailSrcset() {
+      const src = this.thumbnail;
+      return `${src}?w=180 1x, ${src}?w=360 2x, ${src}?w=540 3x`;
+    },
+  },
+  mounted() {
+    this.$store.dispatch('works/getItems');
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.work-detail {
+  height: 100%;
+  width: 100%;
+
+  .image-wrap {
+    height: 240px;
+    position: sticky;
+    top: -160px;
+
+    &::before {
+      background: #999;
+      content: '';
+      display: block;
+      height: 100%;
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      z-index: 0;
+    }
+
+    img {
+      height: 100%;
+      object-fit: cover;
+      object-position: top center;
+      opacity: 0.7;
+      width: 100%;
+    }
+
+    h2 {
+      background: rgba(#fff, 0.7);
+      display: inline-block;
+      font: 700 16px/24px 'Noto Sans';
+      margin-right: 32px;
+      padding: 12px 24px 12px 16px;
+      position: absolute;
+      left: 0;
+      bottom: 16px;
+      text-align: left;
+    }
+  }
+
+  .link-list {
+    list-style: none;
+    margin: 16px;
+  }
+  .link-item {
+    & + .link-item {
+      margin-top: 16px;
+    }
+  }
+
+  .main {
+    font: 400 16px/24px 'Noto Sans';
+    padding: 16px 16px 160px;
+
+    * + * {
+      margin-top: 16px;
+    }
+
+    .img-wrap {
+      margin-right: -16px;
+      margin-left: -16px;
+      overflow-x: scroll;
+      padding: 8px 16px;
+    }
+  }
+}
+</style>
--- src/views/Work.vue
+++ src/views/Work.vue
@@ -1,8 +1,29 @@
 <template>
-  <div class="work">
-    <h1>This is an about page</h1>
+  <div class="work" v-if="!loading">
+    <work-detail v-bind="item" />
   </div>
 </template>
+
+<script>
+import WorkDetail from '../components/WorkDetail';
+
+export default {
+  components: {
+    WorkDetail,
+  },
+  computed: {
+    loading() {
+      return this.$store.state.works.loading;
+    },
+    item() {
+      return this.$store.state.works.items.find((item) => item.id === this.$route.params.id);
+    },
+  },
+  mounted() {
+    this.$store.dispatch('works/getItems');
+  },
+};
+</script>
 
 <style lang="scss" scoped>
 .work {
